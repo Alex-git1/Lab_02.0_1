51168d4c6184f033b6fdd9a93a4a1debc960b96f
unknown <mrcricoarnold@gmail.com>
2024-09-16 12:46:48 +0300

    first master commit

---
+++ Lab_02.0_1/Source.cpp
@@ -0,0 +1,43 @@
+// Lab_02.1cpp
+// < Симодейко Олександр >
+// Лабораторна робота 02.1
+// Лінійні програми.
+// Варіант 13
+
+#include <iostream>
+#include <cmath>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double a, b, z1, z2;
+    cout << "Введіть значення a: "; cin >> a;
+    cout << "Введіть значення b: "; cin >> b;
+
+    z1 = (sin(a) + cos(2 * b - a)) / (cos(a) - sin(2 * b - a));
+    double denominator1 = (cos(a) - sin(2 * b - a));
+
+    if (denominator1 == 0) {
+        cout << "Помилка: Ділення на нуль неможливо!" << endl;
+        return 1;
+    }
+    else {
+        cout << "z1 = " << z1 << endl;
+    }
+
+    z2 = (1 + sin(b) * sin(b)) / (cos(b) * cos(b));
+    double denominator2 = (cos(b) * cos(b));
+
+    if (denominator2 == 0) {
+        cout << "Помилка: Ділення на нуль неможливо!" << endl;
+        return 1;
+    }
+    else {
+        cout << "z2 = " << z2 << endl;
+        return 0;
+    }
+}
